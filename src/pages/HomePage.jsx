ac060fe4a0a58b71e6c3c55adced294b9ca55b43
pvbiadmin <pvbi.org@gmail.com>
2023-05-01 10:42:16 +0800

    Configured routing

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React, { Component, Fragment } from 'react';
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>This is our Home Page</h1>
+      </Fragment>
+    )
+  }
+}
