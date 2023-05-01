b7d34c1a7e6a80bd1f0dd9c668018ba3af2d1a69
pvbiadmin <pvbi.org@gmail.com>
2023-05-01 14:22:22 +0800

    Added styling for Featured Products header

--- src/components/home/FeaturedProducts.jsx
+++ src/components/home/FeaturedProducts.jsx
@@ -1,11 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 
 export default class FeaturedProducts extends Component {
   render() {
     return (
-      <div>
-        FeaturedProducts
-      </div>
+      <Fragment>
+        <Container className="text-center" fluid={true}>
+          <div className="section-title text-center mb-55">
+            <h2>FEATURED PRODUCTS</h2>
+            <p>Some Of Our Exclusive Collections</p>
+          </div>
+          <Row>
+            <Col>
+              
+            </Col>
+          </Row>
+        </Container>
+      </Fragment>
     )
   }
 }
