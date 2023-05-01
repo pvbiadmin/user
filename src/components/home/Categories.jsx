5833b7122b84e0d7ab3e53c6281caf2b8698382b
pvbiadmin <pvbi.org@gmail.com>
2023-05-01 17:44:26 +0800

    Added styling for Categories Component

---
+++ src/components/home/Categories.jsx
@@ -0,0 +1,260 @@
+import React, { Component, Fragment } from 'react'
+import { Card, Col, Container, Row } from 'react-bootstrap';
+
+export default class Categories extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Container className="text-center" fluid={true}>
+            <div className="section-title text-center mb-55">
+                <h2>CATEGORIES</h2>
+                <p>Some Of Our Exclusive Collections</p>
+            </div>
+            <Row>
+                <Col key={1} xl={6} lg={6} md={2} sm={12} xs={12}>
+                    <Row>
+                        <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+                            <Card className="h-100 w-100 text-center">
+                                <Card.Body>
+                                    <img 
+                                        className="center"
+                                        src="https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100" 
+                                        alt="" 
+                                    />
+                                    <h5 className="category-name">MOBILE</h5>
+                                    
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+                            <Card className="h-100 w-100 text-center">
+                                <Card.Body>
+                                    <img 
+                                        className="center"
+                                        src="https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100" 
+                                        alt="" 
+                                    />
+                                    <h5 className="category-name">MOBILE</h5>
+                                    
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+                            <Card className="h-100 w-100 text-center">
+                                <Card.Body>
+                                    <img 
+                                        className="center"
+                                        src="https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100" 
+                                        alt="" 
+                                    />
+                                    <h5 className="category-name">MOBILE</h5>
+                                    
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+                            <Card className="h-100 w-100 text-center">
+                                <Card.Body>
+                                    <img 
+                                        className="center"
+                                        src="https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100" 
+                                        alt="" 
+                                    />
+                                    <h5 className="category-name">MOBILE</h5>
+                                    
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Col>
+
+                <Col key={1} xl={6} lg={6} md={2} sm={12} xs={12}>
+                    <Row>
+                        <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+                            <Card className="h-100 w-100 text-center">
+                                <Card.Body>
+                                    <img 
+                                        className="center"
+                                        src="https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100" 
+                                        alt="" 
+                                    />
+                                    <h5 className="category-name">MOBILE</h5>
+                                    
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+                            <Card className="h-100 w-100 text-center">
+                                <Card.Body>
+                                    <img 
+                                        className="center"
+                                        src="https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100" 
+                                        alt="" 
+                                    />
+                                    <h5 className="category-name">MOBILE</h5>
+                                    
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+                            <Card className="h-100 w-100 text-center">
+                                <Card.Body>
+                                    <img 
+                                        className="center"
+                                        src="https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100" 
+                                        alt="" 
+                                    />
+                                    <h5 className="category-name">MOBILE</h5>
+                                    
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+                            <Card className="h-100 w-100 text-center">
+                                <Card.Body>
+                                    <img 
+                                        className="center"
+                                        src="https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100" 
+                                        alt="" 
+                                    />
+                                    <h5 className="category-name">MOBILE</h5>
+                                    
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col key={1} xl={6} lg={6} md={2} sm={12} xs={12}>
+                    <Row>
+                        <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+                            <Card className="h-100 w-100 text-center">
+                                <Card.Body>
+                                    <img 
+                                        className="center"
+                                        src="https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100" 
+                                        alt="" 
+                                    />
+                                    <h5 className="category-name">MOBILE</h5>
+                                    
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+                            <Card className="h-100 w-100 text-center">
+                                <Card.Body>
+                                    <img 
+                                        className="center"
+                                        src="https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100" 
+                                        alt="" 
+                                    />
+                                    <h5 className="category-name">MOBILE</h5>
+                                    
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+                            <Card className="h-100 w-100 text-center">
+                                <Card.Body>
+                                    <img 
+                                        className="center"
+                                        src="https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100" 
+                                        alt="" 
+                                    />
+                                    <h5 className="category-name">MOBILE</h5>
+                                    
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+                            <Card className="h-100 w-100 text-center">
+                                <Card.Body>
+                                    <img 
+                                        className="center"
+                                        src="https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100" 
+                                        alt="" 
+                                    />
+                                    <h5 className="category-name">MOBILE</h5>
+                                    
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Col>
+
+                <Col key={1} xl={6} lg={6} md={2} sm={12} xs={12}>
+                    <Row>
+                        <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+                            <Card className="h-100 w-100 text-center">
+                                <Card.Body>
+                                    <img 
+                                        className="center"
+                                        src="https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100" 
+                                        alt="" 
+                                    />
+                                    <h5 className="category-name">MOBILE</h5>
+                                    
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+                            <Card className="h-100 w-100 text-center">
+                                <Card.Body>
+                                    <img 
+                                        className="center"
+                                        src="https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100" 
+                                        alt="" 
+                                    />
+                                    <h5 className="category-name">MOBILE</h5>
+                                    
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+                            <Card className="h-100 w-100 text-center">
+                                <Card.Body>
+                                    <img 
+                                        className="center"
+                                        src="https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100" 
+                                        alt="" 
+                                    />
+                                    <h5 className="category-name">MOBILE</h5>
+                                    
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+                            <Card className="h-100 w-100 text-center">
+                                <Card.Body>
+                                    <img 
+                                        className="center"
+                                        src="https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100" 
+                                        alt="" 
+                                    />
+                                    <h5 className="category-name">MOBILE</h5>
+                                    
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        </Container>
+      </Fragment>
+    )
+  }
+}
