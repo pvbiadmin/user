f8a0c65ee919fea267495acef362cc93e3c7d4ad
pvbiadmin <pvbi.org@gmail.com>
2023-05-01 18:48:03 +0800

    Added styling for Collection Product Component

--- src/components/home/Collection.jsx
+++ src/components/home/Collection.jsx
@@ -1,11 +1,130 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 
 export default class Collection extends Component {
   render() {
     return (
-      <div>
-        Collection
-      </div>
+      <Fragment>
+        <Container className="text-center" fluid={true}>
+          <div className="section-title text-center mb-55">
+            <h2>PRODUCT COLLECTION</h2>
+            <p>Some Of Our Exclusive Collections</p>
+          </div>
+          <Row>
+            <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="image-box card w-100"> 
+                <img 
+                  className="center w-75"
+                  src="https://rukminim1.flixcart.com/image/416/416/xif0q/mobile/i/y/j/-original-imagkp8fpw35uygd.jpeg?q=70" 
+                  alt="" 
+                />             
+                <Card.Body>
+                  <p className="product-name-on-card">Realme 10 Pro+ 5G</p> 
+                  <p className="product-price-on-card">₹25,999</p>           
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="image-box card w-100"> 
+                <img 
+                  className="center w-75"
+                  src="https://rukminim1.flixcart.com/image/416/416/xif0q/mobile/i/y/j/-original-imagkp8fpw35uygd.jpeg?q=70" 
+                  alt="" 
+                />             
+                <Card.Body>
+                  <p className="product-name-on-card">Realme 10 Pro+ 5G</p> 
+                  <p className="product-price-on-card">₹25,999</p>           
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="image-box card w-100"> 
+                <img 
+                  className="center w-75"
+                  src="https://rukminim1.flixcart.com/image/416/416/xif0q/mobile/i/y/j/-original-imagkp8fpw35uygd.jpeg?q=70" 
+                  alt="" 
+                />             
+                <Card.Body>
+                  <p className="product-name-on-card">Realme 10 Pro+ 5G</p> 
+                  <p className="product-price-on-card">₹25,999</p>           
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="image-box card w-100"> 
+                <img 
+                  className="center w-75"
+                  src="https://rukminim1.flixcart.com/image/416/416/xif0q/mobile/i/y/j/-original-imagkp8fpw35uygd.jpeg?q=70" 
+                  alt="" 
+                />             
+                <Card.Body>
+                  <p className="product-name-on-card">Realme 10 Pro+ 5G</p> 
+                  <p className="product-price-on-card">₹25,999</p>           
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="image-box card w-100"> 
+                <img 
+                  className="center w-75"
+                  src="https://rukminim1.flixcart.com/image/416/416/xif0q/mobile/i/y/j/-original-imagkp8fpw35uygd.jpeg?q=70" 
+                  alt="" 
+                />             
+                <Card.Body>
+                  <p className="product-name-on-card">Realme 10 Pro+ 5G</p> 
+                  <p className="product-price-on-card">₹25,999</p>           
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="image-box card w-100"> 
+                <img 
+                  className="center w-75"
+                  src="https://rukminim1.flixcart.com/image/416/416/xif0q/mobile/i/y/j/-original-imagkp8fpw35uygd.jpeg?q=70" 
+                  alt="" 
+                />             
+                <Card.Body>
+                  <p className="product-name-on-card">Realme 10 Pro+ 5G</p> 
+                  <p className="product-price-on-card">₹25,999</p>           
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="image-box card w-100"> 
+                <img 
+                  className="center w-75"
+                  src="https://rukminim1.flixcart.com/image/416/416/xif0q/mobile/i/y/j/-original-imagkp8fpw35uygd.jpeg?q=70" 
+                  alt="" 
+                />             
+                <Card.Body>
+                  <p className="product-name-on-card">Realme 10 Pro+ 5G</p> 
+                  <p className="product-price-on-card">₹25,999</p>           
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+              <Card className="image-box card w-100"> 
+                <img 
+                  className="center w-75"
+                  src="https://rukminim1.flixcart.com/image/416/416/xif0q/mobile/i/y/j/-original-imagkp8fpw35uygd.jpeg?q=70" 
+                  alt="" 
+                />             
+                <Card.Body>
+                  <p className="product-name-on-card">Realme 10 Pro+ 5G</p> 
+                  <p className="product-price-on-card">₹25,999</p>           
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </Fragment>
     )
   }
 }
