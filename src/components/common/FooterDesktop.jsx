a7f4e8c52c3ff6a81c9c716ed27cc21fdcb12176
pvbiadmin <pvbi.org@gmail.com>
2023-05-03 18:14:00 +0800

    Finalized Footer Desktop

--- src/components/common/FooterDesktop.jsx
+++ src/components/common/FooterDesktop.jsx
@@ -1,5 +1,8 @@
 import React, { Component, Fragment } from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import IconDlApple from "../../assets/images/apple.png";
+import IconDlGoogle from "../../assets/images/google.png";
 
 export default class FooterDesktop extends Component {
   render() {
@@ -21,14 +24,29 @@ export default class FooterDesktop extends Component {
               </Col>
               <Col className="p-2" lg={3} md={3} sm={6} xs={12}>
                 <h5 className="footer-menu-title">THE COMPANY</h5>
+                <Link to="/" className="footer-link">About Us</Link><br/>
+                <Link to="/" className="footer-link">Company Profile</Link><br/>
+                <Link to="/" className="footer-link">Contact Us</Link><br/>
               </Col>
               <Col className="p-2" lg={3} md={3} sm={6} xs={12}>
                 <h5 className="footer-menu-title">MORE INFO</h5>
+                <Link to="/" className="footer-link">How To Purchase</Link><br/>
+                <Link to="/" className="footer-link">Privacy Policy</Link><br/>
+                <Link to="/" className="footer-link">Refund Policy</Link><br/>
               </Col>
               <Col className="p-2" lg={3} md={3} sm={6} xs={12}>
                 <h5 className="footer-menu-title">DOWNLOAD APP</h5>
+                <a href="/"><img src={IconDlGoogle} alt="Google Download" /></a><br/>
+                <a href="/"><img className="mt-2" src={IconDlApple} alt="Apple Download" /></a>
               </Col>
             </Row>
+          </Container>
+          <Container fluid={true} className="text-center m-0 pt-3 pb-1 bg-dark">
+            <Container>
+              <Row>
+                <h6 className="text-white">&copy; Copyright {(new Date().getFullYear())} Prime Ventures Builder, Inc. All Rights Reserved</h6>
+              </Row>
+            </Container>
           </Container>
         </div>        
       </Fragment>
