0056a8c3eccbf0f7ddb907e189bac47b15773378
pvbiadmin <pvbi.org@gmail.com>
2023-05-05 13:08:29 +0800

    Added Cart List Page

--- src/components/common/NavMenuDesktop.jsx
+++ src/components/common/NavMenuDesktop.jsx
@@ -77,9 +77,9 @@ export default class NavMenuDesktop extends Component {
                   </Link>
                   <button className="btn"><i className="h4 fa fa-mobile-alt"></i></button>
                   <Link to="/login" className="h4 btn">LOGIN</Link>
-                  <Button className="cart-btn">
+                  <Link to="/cart" className="cart-btn">
                     <i className="fa fa-shopping-cart"></i>{" "}3 Items
-                  </Button>
+                  </Link>
                 </Col>
               </Row>
             </Container>
