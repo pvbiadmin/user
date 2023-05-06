2d05fe6cf6325308adaf380c7bd096d55928c3b4
pvbiadmin <pvbi.org@gmail.com>
2023-05-06 22:32:49 +0800

    Fetched About Page main raw data from API

--- src/api/AppUrl.jsx
+++ src/api/AppUrl.jsx
@@ -2,4 +2,5 @@ export default class AppUrl {
   static BaseUrl = "http://127.0.0.1:8000/api";
   static VisitorDetails = this.BaseUrl + "/getvisitor";
   static PostContact = this.BaseUrl + "/postcontact";
+  static AllSiteInfo = this.BaseUrl + "/allsiteinfo"
 }
