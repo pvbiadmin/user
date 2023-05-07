6878a14d170405ca9d6bbec8b7ff0021a5d22afa
pvbiadmin <pvbi.org@gmail.com>
2023-05-07 16:47:17 +0800

    Refund Page CSS Placeholer and Session Storage

--- src/components/others/Refund.jsx
+++ src/components/others/Refund.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import React, { Component, Fragment } from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import AppUrl from "../../api/AppUrl";
 import parse from "html-react-parser";
 
@@ -9,22 +11,42 @@ export default class Refund extends Component {
     super();
 
     this.state = {
-      refundPolicy: ""
+      refundPolicy: "",
+      loaderDiv: "",
+      mainDiv: "d-none"
     }
   }
 
   componentDidMount() {
-    axios.get(AppUrl.AllSiteInfo).then(response => {
-      let StatusCode = response.status;
+    let SiteInfoRefundPolicy = sessionStorage.getItem("AllSiteInfo");
 
-      if (StatusCode === 200) {
-        let JsonData = (response.data)[0]["refund_policy"];
+    if (SiteInfoRefundPolicy === null) {
+      axios.get(AppUrl.AllSiteInfo).then(response => {
+        let StatusCode = response.status;
+  
+        if (StatusCode === 200) {
+          let JsonData = (response.data)[0]["refund_policy"];
+  
+          this.setState({
+            refundPolicy: JsonData, 
+            loaderDiv: "d-none",
+            mainDiv: ""
+          });
 
-        this.setState({refundPolicy: JsonData});
-      }
-    }).catch(error => {
-      console.log(error);
-    });
+          sessionStorage.setItem("SiteInfoRefundPolicy", JsonData);
+        } else {
+          toast.error("Something went wrong", {
+            position: "bottom-center"
+          });
+        }
+      }).catch(() => {
+        toast.error("Something went wrong", {
+          position: "bottom-center"
+        });
+      });
+    } else {
+      this.setState({refundPolicy: SiteInfoRefundPolicy});
+    }     
   }
 
   render() {
@@ -33,12 +55,55 @@ export default class Refund extends Component {
         <Container>
           <Row className="p-2">
             <Col className="shadow-sm bg-white mt-2" md={12} lg={12} sm={12} xs={12}>
-              <h4 className="section-title-login">Refund Policy</h4>
-              <p className="section-title-contact">At PVB i-Store, we want our customers to be completely satisfied with their purchases. If you are not satisfied with your order for any reason, we offer a refund or exchange subject to the following terms and conditions.</p>
-              {parse(this.state.refundPolicy)}
+              <div className={this.state.loaderDiv}>
+                <div className="ph-item">
+                  <div className="ph-col-12">                        
+                    <div className="ph-row">                            
+                      <div className="ph-col-4"></div>
+                      <div className="ph-col-8 empty"></div>
+                      <div className="ph-col-6"></div>
+                      <div className="ph-col-6 empty"></div>
+                      <div className="ph-col-12"></div>
+                      <div className="ph-col-12"></div>
+                      <div className="ph-col-12"></div>
+                      <div className="ph-col-12"></div>
+                    </div>
+                  </div>                
+                  <div className="ph-col-12">                        
+                    <div className="ph-row">                            
+                      <div className="ph-col-4"></div>
+                      <div className="ph-col-8 empty"></div>
+                      <div className="ph-col-6"></div>
+                      <div className="ph-col-6 empty"></div>
+                      <div className="ph-col-12"></div>
+                      <div className="ph-col-12"></div>
+                      <div className="ph-col-12"></div>
+                      <div className="ph-col-12"></div>
+                    </div>
+                  </div>                
+                  <div className="ph-col-12">                        
+                    <div className="ph-row">                            
+                      <div className="ph-col-4"></div>
+                      <div className="ph-col-8 empty"></div>
+                      <div className="ph-col-6"></div>
+                      <div className="ph-col-6 empty"></div>
+                      <div className="ph-col-12"></div>
+                      <div className="ph-col-12"></div>
+                      <div className="ph-col-12"></div>
+                      <div className="ph-col-12"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={this.state.mainDiv}>
+                <h4 className="section-title-login">Refund Policy</h4>
+                <p className="section-title-contact">At PVB i-Store, we want our customers to be completely satisfied with their purchases. If you are not satisfied with your order for any reason, we offer a refund or exchange subject to the following terms and conditions.</p>
+                {parse(this.state.refundPolicy)}
+              </div>              
             </Col>
           </Row>
         </Container>
+        <ToastContainer />
       </Fragment>
     )
   }
