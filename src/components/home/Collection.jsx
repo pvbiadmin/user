fe1a0d347b4c93b37153d735f1b2d6d466338c48
pvbiadmin <pvbi.org@gmail.com>
2023-05-08 17:10:12 +0800

    refactored collection list

--- src/components/home/Collection.jsx
+++ src/components/home/Collection.jsx
@@ -21,9 +21,9 @@ export default class Collection extends Component {
   }
 
   render() {
-    const CollectionList = this.state.productData;
+    const collectionList = this.state.productData;
 
-    const myView = CollectionList.map((collectionList, i) => {     
+    const myView = collectionList.map((collectionList, i) => {     
       if (collectionList.special_price === "na") {
         return (
           <Col className="p-0" key={i.toString()} xl={3} lg={3} md={3} sm={6} xs={6}>
