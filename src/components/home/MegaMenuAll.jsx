6f96a317ed192949797be34cd1cac04ff0e95f8a
pvbiadmin <pvbi.org@gmail.com>
2023-05-08 11:49:49 +0800

    Fetched Categories from API for MegaMenuAll

--- src/components/home/MegaMenuAll.jsx
+++ src/components/home/MegaMenuAll.jsx
@@ -1,49 +1,66 @@
 import React, { Component } from "react";
-import IconCat from "../../assets/images/caticon.png";
+import AppURL from "../../api/AppUrl";
+import axios from "axios";
 
 export default class MegaMenuAll extends Component {
-      constructor() {
-        super();
-    
-        this.MegaMenu = this.MegaMenu.bind(this);
-      }
-    
-      componentDidMount() {
-        this.MegaMenu();
-      }
-    
-      MegaMenu() {
-        const acc = document.getElementsByClassName("accordionAll");
-        const accNum = acc.length;
-    
-        for(let i = 0; i < accNum; i++) {
-          acc[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-    
-            const panel = this.nextElementSibling;
-    
-            if (panel.style.maxHeight) {
-              panel.style.maxHeight = null;
-            } else {
-              panel.style.maxHeight = panel.scrollHeight + "px";
-            }
-          });
-        }
-      }
-      
+  constructor(){
+    super();
+
+    this.state = {
+      MenuData: []
+    }
+  }
+
+  componentDidMount(){
+    axios.get(AppURL.AllCategoryDetails).then(response => { 
+      this.setState({
+        MenuData: response.data
+      });
+    }).catch();
+  }
+
+  MenuItemClick = (event) => {
+    event.target.classList.toggle("active");
+
+    const panel = event.target.nextElementSibling;
+
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px"
+    }
+  }
+
   render() {
-    return (
-      <div className="accordionMenuDivAll">
-        <div className="accordionMenuDivInsideAll">
-          <button className="accordionAll">
-            <img className="accordionMenuIconAll" src={IconCat} alt="" /> Men's Clothing
+    const CatList = this.state.MenuData;
+
+    const MyView = CatList.map((CatList, i) => {
+      return (
+        <div key={i.toString()}>
+          <button onClick={this.MenuItemClick} className="accordionAll">
+            <img className="accordionMenuIconAll" src={CatList.category_image} alt="" />&nbsp;{CatList.category_name}
           </button>
           <div className="panelAll">
             <ul>
-              <li><a href="/" className="accordionItemAll">Men's T-Shirt 1</a></li>
-              <li><a href="/" className="accordionItemAll">Men's T-Shirt 2</a></li>
+              {
+                (CatList.subcategory_name).map((SubList, i)=>{
+                  return (
+                    <li key={i.toString()}>
+                      <a href="/" className="accordionItemAll">{SubList.subcategory_name}</a>
+                    </li>
+                  )
+                })    
+              }
             </ul>
-          </div>
+          </div> 
+        </div>)
+      }
+    );
+
+    return (
+      <div className="accordionMenuDivAll">
+        <div className="accordionMenuDivInsideAll">
+          {MyView}
         </div>
       </div>
     )
