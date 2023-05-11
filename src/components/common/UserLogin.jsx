c43c10bad0aac3371e8eba086ff3703ac790ad0d
pvbiadmin <pvbi.org@gmail.com>
2023-05-11 17:03:24 +0800

    Fetch User Login Data from API via POST Request

--- src/components/common/UserLogin.jsx
+++ src/components/common/UserLogin.jsx
@@ -1,68 +1,97 @@
-import axios from "axios";
-import React, { Component, Fragment } from "react";
-import { Button, Col, Container, Form, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import AppUrl from "../../api/AppUrl";
-import ImgLogin from "../../assets/images/login.png";
+import React, { Component, Fragment } from 'react'
+import { Container, Row, Col, Form, Button } from 'react-bootstrap'
+import Login from '../../assets/images/login.png'
+import { Link, Redirect } from 'react-router-dom'
+import AppUrl from '../../api/AppUrl';
+import axios from 'axios'
 
 export default class UserLogin extends Component {
+
   constructor() {
     super();
     this.state = {
-      email: "",
-      password: "",
-      message: ""
+      email: '',
+      password: '',
+      message: '',
+      loggedIn: false
     }
   }
 
-  setEmailState(event) {
-    this.setState({ email: event.target.value })
-  }
-
-  setPasswordState(event) {
-    this.setState({ password: event.target.value })
-  }
-
-  formSubmit(event) {
-    event.preventDefault();
+  // Login Form Submit Method 
+  formSubmit = (e) => {
+    e.preventDefault();
     const data = {
       email: this.state.email,
       password: this.state.password
     }
 
     axios.post(AppUrl.UserLogin, data).then(response => {
-      console.log(response);
-    }).catch();// 
+
+      localStorage.setItem('token', response.data.token);
+      this.setState({ loggedIn: true })
+      this.props.setUser(response.data.user);
+
+    }).catch();
+
   }
 
+
+
   render() {
+
+    /// After Login Redirect to Profile Page 
+    if (this.state.loggedIn) {
+      return <Redirect to={'/profile'} />
+    }
+
+    if (localStorage.getItem('token')) {
+      return <Redirect to="/profile" />
+    }
+
+
     return (
       <Fragment>
         <Container>
           <Row className="p-2">
             <Col className="shadow-sm bg-white mt-2" md={12} lg={12} sm={12} xs={12}>
+
               <Row className="text-center">
                 <Col className="d-flex justify-content-center" md={6} lg={6} sm={12} xs={12}>
-                  <Form onSubmit={this.formSubmit} className="onboardForm" >
-                    <h4 className="section-title-login">USER LOGIN</h4>
+                  <Form className="onboardForm" onSubmit={this.formSubmit} >
+                    <h4 className="section-title-login"> USER SING IN </h4>
 
-                    <input onChange={event => this.setEmailState(event)} className="form-control m-2" type="email" placeholder="Enter Your Email" />
-                    <input onChange={event => this.setPasswordState(event)} className="form-control m-2" type="password" placeholder="Enter Your Password" />
-                    <Button type="submit" className=" btn btn-block m-2 site-btn-login">Sign In</Button>
-                    <br /><br /><hr />
-                    <p><b>Forgot your password? <Link to="/forget">Reset Password</Link></b></p>
-                    <p><b>Don't have an account? <Link to="/register">Register Here</Link></b></p>
+                    <input className="form-control m-2" type="email" placeholder="Enter Your Email" onChange={(e) => { this.setState({ email: e.target.value }) }} />
+
+                    <input className="form-control m-2" type="password" placeholder="Enter Your Password" onChange={(e) => { this.setState({ password: e.target.value }) }} />
+
+
+                    <Button type="submit" className="btn btn-block m-2 site-btn-login"> Login </Button>
+
+                    <br></br> <br></br>
+                    <hr />
+                    <p> <b> Forget My Password? </b><Link to="/forget"><b> Froget Password </b> </Link> </p>
+
+                    <p> <b> Don't Have An Account ? </b><Link to="/register"><b> Register </b> </Link> </p>
+
                   </Form>
+
+
                 </Col>
-                <Col className="Desktop p-0 m-0" md={6} lg={6} sm={6} xs={6}>
-                  <img className="onboardBanner" src={ImgLogin} alt="Login" />
+
+                <Col className="p-0 Desktop m-0" md={6} lg={6} sm={6} xs={6}>
+                  <img className="onboardBanner" src={Login} alt="" />
                 </Col>
               </Row>
 
+
+
+
+
+
             </Col>
           </Row>
-        </Container >
-      </Fragment >
+        </Container>
+      </Fragment>
     )
   }
 }
