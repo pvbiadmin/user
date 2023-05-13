f24f05a9e32898b03b767fcc6f6449ea653796f8
pvbiadmin <pvbi.org@gmail.com>
2023-05-13 10:19:46 +0800

    Added Cart List Confirm Order Design

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -13,7 +13,14 @@ export default class Cart extends Component {
       productData: [],
       isLoading: "",
       mainDiv: "d-none",
-      PageRefreshStatus: false
+      PageRefreshStatus: false,
+      PageRedirectStatus: false,
+
+      confirmBtn: "Confirm Order",
+      city: "",
+      payment: "",
+      name: "",
+      address: ""
     }
   }
 
@@ -76,43 +83,112 @@ export default class Cart extends Component {
     });
   }
 
-  render() {
-    const cartList = this.state.productData;
+  nameOnChange = (event) => {
+    let name = event.target.value;
+    this.setState({ name: name })
+  }
 
-    const myView = cartList.map((cart, i) => {
+  addressOnChange = (event) => {
+    let address = event.target.value;
+    this.setState({ address: address })
+  }
+
+  confirmOnClick = () => {
+    let city = this.state.city;
+    let payment = this.state.payment;
+    let name = this.state.name;
+    let address = this.state.address;
+    let email = this.props.user.email;
+
+    if (city.length === 0) {
+      cogoToast.error("Please Select City", { position: 'top-right' });
+    }
+    else if (payment.length === 0) {
+      cogoToast.error("Please Select Payment", { position: 'top-right' });
+    }
+    else if (name.length === 0) {
+      cogoToast.error("Please Select Your Name", { position: 'top-right' });
+    }
+    else if (address.length === 0) {
+      cogoToast.error("Please Select Your Address", { position: 'top-right' });
+    }
+    else {
+      let invoice = new Date().getTime();
+      let MyFromData = new FormData();
+
+      MyFromData.append('city', city)
+      MyFromData.append('payment_method', payment)
+      MyFromData.append('name', name)
+      MyFromData.append('delivery_address', address)
+      MyFromData.append('email', email)
+      MyFromData.append('invoice_no', invoice)
+      MyFromData.append('delivery_charge', "00");
+
+      axios.post(AppUrl.CartOrder, MyFromData).then(response => {
+
+        if (response.data === 1) {
+          cogoToast.success("Order Request Received", { position: 'top-right' });
+          this.setState({ PageRedirectStatus: true })
+        } else {
+          cogoToast.error("Your Request is not done ! Try Aagain", { position: 'top-right' });
+        }
+      }).catch(() => {
+        cogoToast.error("Your Request is not done ! Try Aagain", { position: 'top-right' });
+      });
+    }
+  }
+
+  PageRedirect = () => {
+    if (this.state.PageRedirectStatus === true) {
       return (
-        <Col className="p-1" key={i.toString()} lg={12} md={12} sm={12} xs={12}>
-          <Card >
+        <Redirect to="/orderlist" />
+      )
+    }
+  }
+
+  render() {
+    if (!localStorage.getItem('token')) {
+      return <Redirect to="/login" />
+    }
+
+    const MyList = this.state.productData;
+
+    let totalPriceSum = 0;
+
+    const myView = MyList.map((ProductList, i) => {
+      totalPriceSum = totalPriceSum + parseInt(ProductList.total_price)
+      return (
+        <div key={i.toString()}>
+          <Card>
             <Card.Body>
               <Row>
                 <Col md={3} lg={3} sm={6} xs={6}>
-                  <img className="cart-product-img" src={cart.image} alt="" />
+                  <img className="cart-product-img" src={ProductList.image} alt="" />
                 </Col>
 
                 <Col md={6} lg={6} sm={6} xs={6}>
-                  <h5 className="product-name">{cart.product_name}</h5>
-                  <h6><span className="fw-bold">Quantity:</span> {cart.quantity}</h6>
-                  <p>{cart.size} | {cart.color}</p>
-                  <h6><span className="fw-bold">Price:</span> USD {cart.total_price} ({cart.quantity} * {cart.unit_price})</h6>
+                  <h5 className="product-name">{ProductList.product_name}</h5>
+                  <h6> Quantity: {ProductList.quantity} </h6>
+                  <p>{ProductList.size} | {ProductList.color}</p>
+                  <h6>Price: {ProductList.unit_price} x {ProductList.quantity} = {ProductList.total_price}$</h6>
                 </Col>
 
                 <Col md={3} lg={3} sm={12} xs={12}>
 
-                  {/* <input placeholder="2" className="form-control text-center" type="number" /> */}
+                  <Button onClick={() => this.removeItem(ProductList.id)} className="btn mt-2 mx-1 btn-lg site-btn"><i className="fa fa-trash-alt"></i> </Button>
 
-                  <Button onClick={() => this.removeItem(cart.id)} className="btn mt-2 mx-1 btn-lg site-btn"><i className="fa fa-trash-alt"></i></Button>
+                  <Button onClick={() => this.ItemPlus(ProductList.id, ProductList.quantity, ProductList.unit_price)} className="btn mt-2 mx-1 btn-lg site-btn"><i className="fa fa-plus"></i> </Button>
 
-                  <Button onClick={() => this.itemMinus(cart.id, cart.quantity, cart.unit_price)} className="btn mt-2 mx-1 btn-lg site-btn"><i className="fa fa-minus"></i></Button>
-
-                  <Button onClick={() => this.itemPlus(cart.id, cart.quantity, cart.unit_price)} className="btn mt-2 mx-1 btn-lg site-btn"><i className="fa fa-plus"></i></Button>
+                  <Button onClick={() => this.ItemMinus(ProductList.id, ProductList.quantity, ProductList.unit_price)} className="btn mt-2 mx-1 btn-lg site-btn"><i className="fa fa-minus"></i> </Button>
 
                 </Col>
               </Row>
             </Card.Body>
           </Card>
-        </Col>
+        </div>
       )
-    });
+    })
+
     return (
       <Fragment>
         <Container>
@@ -120,25 +196,61 @@ export default class Cart extends Component {
             <h2>Product Cart List</h2>
           </div>
           <Row>
-            {myView}
+            <Col className="p-1" lg={7} md={7} sm={12} xs={12} >
+              {myView}
+            </Col>
 
-            {/* <Col className="p-1" lg={12} md={12} sm={12} xs={12}>
-              <Card>
-                <Card.Body>
-                  <Row>
-                    <Col md={4} lg={4} sm={6} xs={6}>
-                      <h5><span className="fw-bold">Total Item:</span> 5</h5>
-                      <h5><span className="fw-bold">Total Price:</span> USD 5,000.00</h5>
-                      <Button className="btn btn-block w-100 mt-3  site-btn"><i className="fa fa-shopping-cart"></i> CheckOut </Button>
-                    </Col>
-                  </Row>
-                </Card.Body>
-              </Card>
-            </Col> */}
+            <Col className="p-1" lg={5} md={5} sm={12} xs={12} >
+              <div className="card p-2">
+                <div className="card-body">
+                  <div className="container-fluid ">
+                    <div className="row">
+                      <div className="col-md-12 p-1  col-lg-12 col-sm-12 col-12">
+                        <h5 className="Product-Name text-danger">Total Due: ${totalPriceSum}</h5>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-md-12 p-1 col-lg-12 col-sm-12 col-12">
+                        <label className="form-label">City</label>
+                        <select onChange={this.cityOnChange} className="form-control">
+                          <option value="">Choose Your City</option>
+                          <option value="Manila">Manila</option>
+                          <option value="Mandaluyong">Mandaluyong</option>
+                          <option value="Makati">Makati</option>
+                          <option value="Pasig">Pasig</option>
+                          <option value="Marikina">Marikina</option>
+                          <option value="Antipolo">Antipolo</option>
+                        </select>
+                      </div>
+                      <div className="col-md-12 p-1 col-lg-12 col-sm-12 col-12">
+                        <label className="form-label">Payment Method</label>
+                        <select onChange={this.paymentMethodOnChange} className="form-control">
+                          <option value="">Choose Method</option>
+                          <option value="Cash On Delivery">Cash On Delivery</option>
+                          <option value="Cash On Delivery">Stripe</option>
+                        </select>
+                      </div>
+                      <div className="col-md-12 p-1 col-lg-12 col-sm-12 col-12">
+                        <label className="form-label">Name</label>
+                        <input onChange={this.nameOnChange} className="form-control" type="text" placeholder="" />
+                      </div>
+
+                      <div className="col-md-12 p-1 col-lg-12 col-sm-12 col-12">
+                        <label className="form-label">Delivery Address</label>
+                        <textarea onChange={this.addressOnChange} rows={2} className="form-control" type="text" placeholder="" />
+                      </div>
+                      <div className="col-md-12 p-1 col-lg-12 col-sm-12 col-12" style={{ display: 'flex', justifyContent: 'center' }}>
+                        <button onClick={this.confirmOnClick} className="btn  site-btn"> {this.state.confirmBtn} </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Col>
           </Row>
         </Container>
         {this.PageRefresh()}
-      </Fragment>
+      </Fragment >
     )
   }
 }
